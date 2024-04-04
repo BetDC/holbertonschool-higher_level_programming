ebc64ea2440a9178d14a7b5e548738054e34798d
Beto <beto@MacBook-Pro-de-Beto.local>
2024-04-03 21:01:36 -0600

    Correction number 5

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
     if i != 101 and i != 113:  # ASCII de 'e' es 101, y de 'q' es 113
-        print(f"{chr(i)}", end="")
+        print("{}".format(chr(i)), end="")
